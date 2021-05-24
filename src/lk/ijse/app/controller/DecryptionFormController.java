196b09ec8f6f42da0ba17f4c9c9237e7c69cff9c
Gayal Seneviratne <gayals40@gmail.com>
2021-05-24 14:32:26 +0530

    Util package created

--- src/lk/ijse/app/controller/DecryptionFormController.java
+++ src/lk/ijse/app/controller/DecryptionFormController.java
@@ -1,13 +1,38 @@
 package lk.ijse.app.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import lk.ijse.app.util.Dep7Crypto;
 
 public class DecryptionFormController {
-    public TextField txtCiper;
+    public TextField txtCipher;
     public TextField txtKey;
     public TextField txtText;
 
     public void btnDecrypt_OnAction(ActionEvent actionEvent) {
+        String cipherText = txtCipher.getText();
+        String key  = txtKey.getText();
+
+        if(cipherText.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid cipher", ButtonType.OK).show();
+            txtCipher.requestFocus();
+            return;
+        }
+
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        txtText.setText(Dep7Crypto.decrypt(cipherText,key));
+
+
+
+
+
+
     }
 }
--- src/lk/ijse/app/controller/EncryptionFormController.java
+++ src/lk/ijse/app/controller/EncryptionFormController.java
@@ -1,13 +1,34 @@
 package lk.ijse.app.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TextField;
+import lk.ijse.app.util.Dep7Crypto;
 
 public class EncryptionFormController {
-    public TextField textText;
+    public TextField txtText;
     public TextField txtKey;
-    public TextField txtCiper;
+    public TextField txtCipher;
 
-    public void btnEncript_OnAction(ActionEvent actionEvent) {
+    public void btnEncrypt_OnAction(ActionEvent actionEvent) {
+        String text = txtText.getText();
+        String key  = txtKey.getText();
+
+        if(text.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid text", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        if(key.trim().isEmpty()){
+            new Alert(Alert.AlertType.ERROR, "Please enter valid key", ButtonType.OK).show();
+            txtText.requestFocus();
+            return;
+        }
+
+        txtCipher.setText(Dep7Crypto.encrypt(text,key));
+
+
     }
 }
